e4a7a6fedcdc8632566d54d9f2a341268c78d31e
NasAtanasov <nasatanasov84@egmail.com>
2023-01-26 22:25:51 +0200

    first commit

---
+++ test/specs/contact-us.spec.js
@@ -0,0 +1,53 @@
+
+describe('webdriveruniversity - contact us page', () => {
+
+    beforeEach(async() => {
+        await browser.maximizeWindow();
+        // navigate to a new URL
+        await browser.url('/Contact-Us/contactus.html')
+        console.log(`>>Browser Object: ${JSON.stringify(browser)}`);
+    });
+
+    it('valid submission - submit all information', async () => { 
+        
+        const firstName = await $('//*[@name="first_name"]');
+        await firstName.setValue("Bugs");
+      
+        const lastName = await $('//*[@name="last_name"]');
+        await lastName.setValue("Bunny");
+        
+        const email = await $('//*[@name="email"]');  
+        await email.setValue("Bugs.Bunny@example.com");      
+        
+        const comment = await $('//*[@name="message"]'); 
+        await comment.setValue("Hi there I'm Bugs Bunny\nWhat's up dog!"); 
+        
+        const submitBtn = await $('//input[@value="SUBMIT"]');
+        await submitBtn.click();
+       
+        const successfulSubmissionHeader = $('#contact_reply > h1');
+        await expect(successfulSubmissionHeader).toHaveText('Thank You for your Message!');       
+    });
+    
+    it('invalid submission - dont submit all information', async() => {       
+        
+        const firstName = await $('//*[@name="first_name"]');
+        await firstName.setValue("Bugs");
+      
+        const lastName = await $('//*[@name="last_name"]');
+        await lastName.setValue("Bunny");
+        
+        const email = await $('//*[@name="email"]');  
+        await email.setValue("Bugs.Bunnyexample.com");      
+        
+        const comment = await $('//*[@name="message"]'); 
+        await comment.setValue("Hi there I'm Bugs Bunny\nWhat's up dog!"); 
+        
+        const submitBtn = await $('//input[@value="SUBMIT"]');
+        await submitBtn.click();
+       
+        const successfulSubmissionHeader = $('body');
+        await expect(successfulSubmissionHeader).toHaveTextContaining(['Error: Invalid email address'], ['Error: all fields are required']); 
+    }); 
+
+});
