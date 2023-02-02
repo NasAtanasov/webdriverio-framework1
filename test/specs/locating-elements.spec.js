f6e1aabdca9eafd8720c789af789f2c2a88c2fae
Nas <nasatanasov84@gmail.com>
2023-02-02 16:13:33 +0200

    locating-elements

--- test/specs/locating-elements.spec.js
+++ test/specs/locating-elements.spec.js
@@ -1,11 +1,10 @@
 describe('locating elements', () => {
     beforeEach(async () =>{
-        await browser.maximizeWindow();   
+        await browser.maximizeWindow();
+        await browser.url("https://selectors.webdriveruniversity.com/");   
     })
   
     it('$ - locate element', async() => {
-        await browser.url("https://selectors.webdriveruniversity.com/");
-
         await browser.$("//a[@href='#portfolio']").click();
         await browser.pause(3000);
 
@@ -13,6 +12,32 @@ describe('locating elements', () => {
         const webdriveioButton = await $("[data-target='#portfolioModal1']");
         await webdriveioButton.click();
         await browser.pause(3000);
-
     });
+
+    it.only('$$ - locate elements', async() => {
+        const expectedTitles = [
+            "#",
+            "First",
+            "Last",
+            "Handle",
+            "1",
+            "2",
+            "3",
+            "Firstname",
+            "Firstname",
+            "Lastname",
+            "Age"
+
+        ]
+        const actualTitles = [];
+        const tableHeaderTitles = await $$('//table//th');
+        for(const title of tableHeaderTitles){
+            //console.log(await title.getText());
+            actualTitles.push(await title.getText());
+        }
+
+        expect(expectedTitles).toEqual(actualTitles);
+
+    });   
+        
 });
