21f61f5c5eca6035c471739bb7fe4096781c4647
Nas <nasatanasov84@gmail.com>
2023-03-03 23:35:53 +0200

    itemPrice.push to array

--- test/specs/add-items-to-basket.spec.js
+++ test/specs/add-items-to-basket.spec.js
@@ -8,12 +8,15 @@ describe('add items to basket', () => {
         await skincareLinks[1].click();
 
         const skincareProducts_Header_Links = await $$('.fixed_wrapper .prdocutname');
+        const itemPrices = [];
 
         for (const header of skincareProducts_Header_Links) {
             const tempHeaderText = await header.getText();
 
             if (tempHeaderText.toLowerCase() == "Creme Precieuse Nuit 50ml".toLowerCase() || tempHeaderText.toLowerCase() == "Total Moisture Facial Cream".toLowerCase()) {
+
                 const attr = await header.getAttribute("href");
+
                 //console.log(attr);
                 //https://automationteststore.com/index.php?rt=product/product&path=43&product_id=93
                 //https://automationteststore.com/index.php?rt=product/product&path=43&product_id=66
@@ -24,6 +27,12 @@ describe('add items to basket', () => {
                 //a[@data-id="66"]
                 //a[@data-id="93"]
                 await $(`//a[@data-id='${itemId}']`).click();
+
+                // //a[@data-id="93"]/following-sibling::div/div[@class="pricenew"] | //a[@data-id="66"]/following-sibling::div/div[@class="oneprice"]
+                itemPrices.push(
+                    await $(`//a[@data-id='${itemId}']/following-sibling::div/div[@class="pricenew"]` + ` | //a[@data-id='${itemId}']/following-sibling::div/div[@class="oneprice"]`).getText()
+                )
+
             }
         }
         await browser.pause(5000);
