cbacc5cea12dc6b34ada63530f360d2b9c7e1f59
NasAtanasov <nasatanasov84@gmail.com>
2023-04-14 16:44:13 +0300

    clear some comments

--- test/pageObjects/automation-test-store/components/header-nav.comp.js
+++ test/pageObjects/automation-test-store/components/header-nav.comp.js
@@ -2,8 +2,5 @@ class HeaderNavComponent{
     get cartLink() {
         return $(`//span[text()='Cart']`);
     }
-    // topNavBarLink(linkText) {
-    //     return $$(`//span[contains(text(), '${linkText}')]`);
-    // }
 }
 export default new HeaderNavComponent();
